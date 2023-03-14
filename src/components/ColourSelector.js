adb919b04bcce0dbf36cc051c729ffae2391428d
Ghimirevivek <stephengmre@gmail.com>
2023-03-14 16:54:54 +0530

    paintapp

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
-  const { background } = config;
+  const { label, className, background } = config
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <button
+      className={className /* classname should come here */}
+      onClick={() => selectNextBackground({ background: background })}
+    >
+      {label /* label should come here */}
     </button>
   )
 }
-export default ColourSelector;
+export default ColourSelector
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,15 @@
+import React, { useState } from 'react'
 
+const Selection = ({ applyColor }) => {
+  const [style, setStyle] = useState({ background: '' })
+
+  const handleClick = () => {
+    applyColor(setStyle)
+  }
+  return (
+    <div className="fix-box" style={style} onClick={handleClick}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  )
+}
+export default Selection
